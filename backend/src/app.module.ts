f82669c2d2d04836c691d67e5ae362b689a99b54
Daniel Otavio Todt <danieltodt@gmail.com>
2023-08-17 18:49:01 -0300

    refactor: modules registered

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AuthModule } from '@auth/auth.module';
+import { CommonModule } from '@common/common.module';
+import { ConferencesModule } from '@conferences/conferences.module';
+import { TalksModule } from '@talks/talks.module';
+import { QuestionsModule } from '@questions/questions.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CommonModule,
+    AuthModule,
+    ConferencesModule,
+    TalksModule,
+    QuestionsModule,
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Perguntando')
+    .setDescription('API')
+    .setVersion('1.0')
+    .addTag('conferences')
+    .addTag('talks')
+    .addTag('questions')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Perguntando API',
+    swaggerOptions: {
+      deepLink: true,
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
