e6b09d87905cd15431f2ca6973cc8c20aab9625d
Daniel Otavio Todt <danieltodt@gmail.com>
2023-08-19 07:37:45 -0300

    feat: auth token http interceptor

--- mobile/lib/src/app_module.dart
+++ mobile/lib/src/app_module.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:uno/uno.dart';
 
 import 'features/auth/auth_module.dart';
+import 'features/auth/interactor/atoms/auth_atoms.dart';
+import 'features/auth/interactor/states/auth_state.dart';
 import 'features/conferences/conferences_module.dart';
 import 'features/splash/ui/pages/splash_page.dart';
 
 class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addSingleton<Uno>(() {
+      final uno = Uno();
+      uno.interceptors.request.use(resolveRequest);
+      return uno;
+    });
+  }
+
   @override
   List<Module> get imports => [
         AuthModule(),
@@ -15,5 +27,14 @@ class AppModule extends Module {
     r.child('/splash', child: (_) => const SplashPage());
     r.module('/conferences', module: ConferencesModule());
     r.module('/auth', module: AuthModule());
+  }
+
+  Request resolveRequest(Request request) {
+    final state = authState.value;
+    if (state is Logged) {
+      final token = state.token;
+      request.headers['Authorization'] = 'Bearer $token';
+    }
+    return request;
   }
 }
