c37849e421503b65728dbe1bc4dafb6e8012e735
Daniel Otavio Todt <danieltodt@gmail.com>
2023-08-27 14:18:24 -0300

    feat[mobile]: crashlytics

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:asp/asp.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:perguntando/src/app_module.dart';
@@ -13,6 +15,17 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (!kIsWeb) {
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
+    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
 
   runApp(
     RxRoot(
